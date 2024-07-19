6b0a1910516fca64bbe768c3d424ac40a52386cb
net11FellaJ <julian.fella@neteleven.de>
2024-07-19 02:03:47 +0200

    initial commit

--- app/src/client/type.ts
+++ app/src/client/type.ts
@@ -1,1 +1,16 @@
 export type colorOptions = 'primary' | 'secondary' | 'accent'
+
+export type StrapiImage = {
+  data: ImageData
+}
+
+type ImageData = {
+  attributes: ImageAttributes
+}
+
+type ImageAttributes = {
+  name: string
+  alternativeText: string
+  caption: string
+  url: string
+}
